937dd45515f265c075b8351f37e06b25d4d2ac6f
Spes <bloomspes@gmail.com>
2021-01-05 18:15:21 +0900

    Time Limit Exceeded

--- Leetcode/Maximum_Number_of_Eaten_Apples.py
+++ Leetcode/Maximum_Number_of_Eaten_Apples.py
@@ -7,13 +7,34 @@
 # 2 day = 첫 째날에 남아있는 사과는 모두 썩는다. 사과가 5개가 열렸고 1개를 먹는다.
 # 3 day = 둘 째날에 열린 사과 1개를 먹는다.
 
-# 이렇게 내가 먹을 수 있는 사과의 갯수 n = 3개이다.
-# 열리는 사과의 수와 사과가 썩는 날을 파악해서
-# 썩은 사과는 내보내고, 남아있는 사과를 계산하는 알고리즘을 작성한다.
+# 내가 먹을 수 있는 사과의 갯수를 최대로 늘리려면 어떻게 해야 할까?
+# 사과가 썩는 날은 i와 days[i]의 영향을 받는다.
+# 그러므로 사과의 freshness(== 썩기까지 남은 시간)가 가장 떨어지는 사과부터 먹는다.
+# freshness를 기준점으로 min-heap 구현
+
+
+from heapq import heappush as push
+from heapq import heappop as pop
 
 class Solution:
     def eatenApples(self, apples: List[int], days: List[int]) -> int:
-
+        
+        # 최소 힙 구현
+        heap = []
+        i= 0
         ans = 0
-        pq = []
-        rotting_apples = list(zip(apples, days))
+        
+        while True:
+            if i < len(apples) and apples[i] > 0:
+                push(heap, [i+days[i], apples[i]])
+            
+            while heap and (heap[0][0] <= i or heap[0][1] <= 0):
+                pop(heap)
+            if heap:
+                heap[0][1] -= 1
+                ans += 1
+            
+            i += 1
+        
+        return ans
+            
